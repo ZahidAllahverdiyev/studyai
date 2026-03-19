b3b0c2560db538641cc83e90332b0203daa957a1
Zahid Allahverdiyev <strikecraft100@gmail.com>
2026-03-20 01:18:09 +0400

    Fix unused import

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,7 +2,7 @@
 // src/pages/LoginPage.js
 // ============================================================
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import toast from 'react-hot-toast';
