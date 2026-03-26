53c78d8fe25142f832d89075a44749903aebc089
Zahid Allahverdiyev <strikecraft100@gmail.com>
2026-03-27 02:41:47 +0400

    fix avatar cors

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,9 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.set('trust proxy', 1);
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: { policy: "cross-origin" }
+}));
 
 app.use(cors({
   origin: [
